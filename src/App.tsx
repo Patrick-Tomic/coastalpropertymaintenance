e5e75b3ec759a46073075e91fd6d9247f767c100
Patrick Emanuel Tomic <pat12tomic@gmail.com>
2026-05-05 11:11:05 -0400

    anotha one

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
+
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => { window.scrollTo(0, 0) }, [pathname])
+  return null
+}
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
@@ -10,6 +17,7 @@ import Contact from './pages/Contact'
 export default function App() {
   return (
     <div className="flex flex-col min-h-screen">
+      <ScrollToTop />
       <Navbar />
       <main className="flex-grow">
         <Routes>
